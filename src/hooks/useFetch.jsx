108c665d155316af5c2b10a82291a30dc27a6fa7
Pedro H. Rocha <pedro_6568@hotmail.com>
2023-07-04 10:23:38 -0300

    Corrigindo responsividade

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -5,17 +5,18 @@ export const useFetch = (url) => {
    const [data, setData] = useState(null)
 
    useEffect(() => {
-      fetch(url, {
-         method:"GET",
-         headers: {
-            'Content-Type':'application/json'
-         }
-      })
-      .then((resp) => resp.json())
-      .then((data) => {
-         setData(data)
-      })
-      .catch((err) => console.log(err))      
+
+      async function fetchData() {
+
+         const res = await fetch(url)
+         const json = await res.json()
+
+         setData(json)
+      }
+
+      fetchData()
+
    }, [url])
+
    return {data}
 }
